45a6ec9fc9ab75dbf007c24e662db7f351b08abd
Administrator <Administrator@DESKTOP-M1ME0BS>
2025-04-08 17:39:05 +0900

    초기 커밋.

---
+++ WpfExplorer/App.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Jamesnet.Wpf.Controls;
+
+namespace WpfExplorer
+{
+    internal class App : JamesApplication
+    {
+        protected override Window CreateShell()
+        {
+            // Temporary Window instance
+            return new Window();
+        }
+    }
+}
